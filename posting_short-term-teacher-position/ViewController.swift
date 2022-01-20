73c618a593e8c7fcb3e1a249ede80e9aa4816add
SeungHee Seo <https://github.com/blingsh>
2022-01-20 15:01:04 +0900

    Initial Commit

---
+++ posting_short-term-teacher-position/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  posting_short-term-teacher-position
+//
+//  Created by Seo on 2022/01/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
