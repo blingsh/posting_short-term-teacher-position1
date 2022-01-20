11e1e519c9015b5f4f429e1c3ef6feb51dae41e0
SeungHee Seo <https://github.com/blingsh>
2022-01-20 15:49:43 +0900

    2

--- posting_short-term-teacher-position/ViewController.swift
+++ posting_short-term-teacher-position/ViewController.swift
@@ -13,7 +13,16 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    class Posting {
+        var title: String
+        var detail: String
+        var duedate: String
+    }
+    class PostingCell {
+        @IBOutlet var title: UILabel!
+        @IBOutlet var detail: UILabel!
+        @IBOutlet var duedate: UILabel!
+    }
 
 }
 
